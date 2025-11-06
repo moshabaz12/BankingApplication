bf8ac71ccbc118a8e18f6ef72439844c8a8dbd4c
moshabaz12 <shabazkhan68224@gmail.com>
2025-11-06 06:44:59 -0800

    BankingApplication

--- BankingApplication/src/main/java/com/BankingApplication/service/AccountService.java
+++ BankingApplication/src/main/java/com/BankingApplication/service/AccountService.java
@@ -1,10 +1,21 @@
 package com.BankingApplication.service;
 
-import com.BankingApplication.dto.AccountDto;
+import java.util.List;
 
+import com.BankingApplication.dto.AccountDto;
 
 public interface AccountService {
 	AccountDto createAccount(AccountDto account);
 
 	AccountDto getAccountById(Long id);
+
+	AccountDto deposit(Long id, double amount);
+
+	AccountDto withdraw(Long id, double amount);
+	
+//	AccountDto updateAccount(Long id, AccountDto accountDto);
+
+	List<AccountDto> getAllAccounts();
+
+	void deleteAccount(Long id);
 }
--- BankingApplication/src/main/java/com/BankingApplication/service/impl/AccountServiceImp.java
+++ BankingApplication/src/main/java/com/BankingApplication/service/impl/AccountServiceImp.java
@@ -1,6 +1,10 @@
 package com.BankingApplication.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import com.BankingApplication.dto.AccountDto;
@@ -8,27 +12,75 @@ import com.BankingApplication.entity.Account;
 import com.BankingApplication.mapper.AccountMapper;
 import com.BankingApplication.repository.AccountRepository;
 import com.BankingApplication.service.AccountService;
+
 @Service
 
-public class AccountServiceImp  implements AccountService{
+public class AccountServiceImp implements AccountService {
+
 	@Autowired
-private AccountRepository accountRepository;
+	private AccountRepository accountRepository;
+
 	public AccountServiceImp(AccountRepository accountRepository) {
 		super();
 		this.accountRepository = accountRepository;
-		
+
 	}
+
 	@Override
 	public AccountDto createAccount(AccountDto accountDto) {
 		Account account = AccountMapper.mapToAccount(accountDto);
 		Account savedAccount = accountRepository.save(account);
-		
+
 		return AccountMapper.mapToAccountDto(savedAccount);
 	}
+
 	@Override
 	public AccountDto getAccountById(Long id) {
-		Account account = accountRepository.findById(id).orElseThrow(()->new  RuntimeException("Account does not exist"));
+		Account account = accountRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account does not exist"));
 		return AccountMapper.mapToAccountDto(account);
 	}
 
+	@Override
+	public AccountDto deposit(Long id, double amount) {
+		Account account = accountRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account does not exist"));
+		double totallBalance = account.getBalance() + amount;
+		account.setBalance(totallBalance);
+		Account savedAccount = accountRepository.save(account);
+
+		return AccountMapper.mapToAccountDto(savedAccount);
+	}
+
+	@Override
+	public AccountDto withdraw(Long id, double amount) {
+		Account account = accountRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account does not exist"));
+		if (account.getBalance() < amount) {
+			throw new RuntimeException("Insufficient Balance");
+		}
+		double totalBlance = account.getBalance() - amount;
+		account.setBalance(totalBlance);
+		Account savedAccount = accountRepository.save(account);
+		return AccountMapper.mapToAccountDto(savedAccount);
+	}
+
+	@Override
+	public List<AccountDto> getAllAccounts() {
+		 return accountRepository.findAll().stream().map((account)->AccountMapper.mapToAccountDto(account))
+				 .collect(Collectors.toList());
+		
+	}
+
+	@Override
+	public void deleteAccount(Long id) {
+		Account account = accountRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account does not exist"));
+//		return AccountMapper.mapToAccountDto(account);
+		
+		accountRepository.delete(account);
+		
+	}
+
+	
 }
